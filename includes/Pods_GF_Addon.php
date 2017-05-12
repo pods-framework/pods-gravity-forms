9ebb8ae5b56e031e6d359d7a957d3bd63930148c
richardW8k <richard@wawrzyniak.me>
2017-05-12 17:37:22 +0100

    Fix issue with forms embedded on the dashboard

--- includes/Pods_GF_Addon.php
+++ includes/Pods_GF_Addon.php
@@ -418,12 +418,14 @@ class Pods_GF_Addon extends GFFeedAddOn {
 
 	}
 
-	public function init_frontend() {
+	public function init() {
 
-		parent::init_frontend();
+		parent::init();
 
-		add_filter( 'gform_pre_render', array( $this, '_gf_pre_render' ) );
-		add_action( 'gform_pre_process', array( $this, '_gf_pre_process' ) );
+		if ( $this->is_gravityforms_supported() ) {
+			add_filter( 'gform_pre_render', array( $this, '_gf_pre_render' ) );
+			add_action( 'gform_pre_process', array( $this, '_gf_pre_process' ) );
+		}
 
 	}
 
